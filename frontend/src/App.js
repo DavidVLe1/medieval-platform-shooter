e6aa752d2a2798ec1fccab6403e2e76e7fbdf45b
David Le <le.davidv18@gmail.com>
2023-10-20 22:27:10 -0500

    instantiated phaser game example on frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import PhaserGame from "./components/PhaserGame";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>My React + Phaser Game</h1>
+        <PhaserGame />
+      </div>
+      {/* <Router>
+
+      </Router> */}
+    </>
+
   );
 }
 
